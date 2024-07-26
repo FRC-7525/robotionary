e6590339b23d5dc6d99ab9a2ac6c893652c8fc26
saturnaliam <luciacdev@gmail.com>
2024-07-26 08:38:12 -0500

    removed MEAN HATEFUL comment

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,12 @@ import DrawingBox from './components/DrawingBox'
 import Toolbar from './components/Toolbar';
 
 function App() {
-  // this code lowkey sucks
   const canvasRef = useRef(null);
 
   return (
     <>
-    <DrawingBox width={345} height={345} canvasRef={canvasRef} />
-    <Toolbar canvasRef={canvasRef} />
+      <DrawingBox width={345} height={345} canvasRef={canvasRef} />
+      <Toolbar canvasRef={canvasRef} />
     </>
   )
 }
