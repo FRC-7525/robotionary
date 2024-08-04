52ed6b3fa7c50f6e38b908911406338cbf737d43
Nanobot567 <70984367+Nanobot567@users.noreply.github.com>
2024-08-03 21:42:18 -0500

    merge titlescreen into main (#2)
    * first basic prototype
    
    * you wouldn\'t rewrite your entire project in one commit
    
    * matchresults stuff
    
    * titlescreen

---
+++ src/components/TitleScreen.tsx
@@ -0,0 +1,14 @@
+function TitleScreen() {
+  return (
+    <>
+      <h1> Robotionary </h1> 
+
+      <div>
+        <input type="text" placeholder="Name"></input>
+        <button> Go </button>
+      </div>
+    </>
+  );
+};
+
+export default TitleScreen;
