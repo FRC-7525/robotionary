fde928270003de08bf00c3524a3eb6c9465d8f33
Nanobot567 <567.nanobot@gmail.com>
2024-08-08 22:08:44 -0500

    basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import { useRef } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DrawingBox from './components/DrawingBox'
 import Toolbar from './components/Toolbar';
-import ViewBox from './components/ViewBox';
+import TitleScreen from './components/TitleScreen';
 
 function App() {
   const canvasRef = useRef(null);
   return (
-    <>
-      <DrawingBox width={345} height={345} canvasRef={canvasRef} />
-      <Toolbar canvasRef={canvasRef} />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TitleScreen />}> </Route>
+        <Route path="drawing" element={
+          <>
+            <DrawingBox width={345} height={345} canvasRef={canvasRef} />
+            <Toolbar canvasRef={canvasRef} />
+          </>
+        } />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
